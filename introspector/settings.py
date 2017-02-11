54c35606fe81dbc2c007541a5fa80e16005249d3
james mike dupont <jamesmikedupont@gmail.com>
2017-02-11 17:23:01 -0500

    update

--- introspector/settings.py
+++ introspector/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'e+k)v61hnyqk0^6l3z(5!(lxn^*pdrhgt_+fwv1yas%36-5_ng'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = [ 'www.basementdweller.club' ]
 
 
 # Application definition
